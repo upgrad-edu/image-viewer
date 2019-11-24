6a33507a8c0088ec0d47b7701bfad882188e0e14
Rahul <rahul7751@gmail.com>
2019-11-25 00:24:02 +0530

    login page ui design completed

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
 import './Login.css';
 import Header from '../../common/header/Header';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
 
 class Login extends Component {
 	render(){
@@ -10,8 +17,31 @@ class Login extends Component {
 		<div className="app-header">
 		<Header heading="Image Viewer"/><br />
 		</div>
-		</div>
 
-		)}
+		<div className="card-container">
+        <Card className="cardStyle">
+        	<CardContent>
+                <Typography variant="h5" component="h5">
+        		LOGIN
+        		</Typography>
+
+        		<FormControl required>
+        			<InputLabel htmlFor="username">Username</InputLabel>
+        			<Input id="username" type="text"/>
+        		</FormControl>
+        		<br /><br />
+
+        		<FormControl required>
+                    <InputLabel htmlFor="loginPassword">Password</InputLabel>
+                    <Input id="loginPassword" type="password"/>
+        		</FormControl>
+        		<br /><br />
+
+        		<Button variant="contained" color="primary">LOGIN</Button>
+        	</CardContent>
+        </Card>
+      </div>
+	</div>
+ )}
 }
 export default Login;
