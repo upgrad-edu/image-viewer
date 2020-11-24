bf0de4766416ea2bb87c6ff9dd9ceb107140b4e5
maniyedidi <maniyedidi@gmail.com>
2020-11-24 16:15:20 +0530

    login page implementation (#1)

---
+++ src/common/header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <span className="app-logo">Image Viewer</span>
+    </div>
+  );
+};
+
+export default Header;
