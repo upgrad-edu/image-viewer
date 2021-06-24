900536423dc9cd353c3f56bcda309d6cdef746b3
Bhargava Raju <BhargavaR@anthology.com>
2021-06-25 04:50:38 +0530

    Creating the common header component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './common/Header';
 import './index.css';
 
 ReactDOM.render(
-    <span>
-        Image Viewer
-    </span>, 
+    <Header/>, 
     document.getElementById('root')
 );
