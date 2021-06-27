10ecb4325d56ef91a3b09baa0fe9677907f108d9
Bhargava Raju <BhargavaR@anthology.com>
2021-06-27 18:18:07 +0530

    Adding home page to the project

---
+++ src/screens/home/Home.js
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import React from 'react'
+import './Header.css'
+
+class Home extends Component {   
+
+    render() {
+        return (
+            <div class="main-header">
+                <div class="main-header-logo">
+                    <span id="main-header-logo-text">Image Viewer</span>
+                </div>
+                <div class="main-header-searcher">
+                    <div>
+                       
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Home;
