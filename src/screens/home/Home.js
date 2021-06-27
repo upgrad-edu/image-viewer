f58babb0c1009864ee7508c0c2e6d5dd9056e5ff
Bhargava Raju <BhargavaR@anthology.com>
2021-06-28 00:12:06 +0530

    Integrated the handler to the home component

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,6 +1,24 @@
 import React, {Component} from 'react';
 import './Home.css';
 import Header from '../../common/header/Header';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import Avatar from '@material-ui/core/Avatar';
+import {withStyles} from '@material-ui/core/styles';
+import FavoriteIconBorder from '@material-ui/icons/FavoriteBorder';
+import FavoriteIconFill from '@material-ui/icons/Favorite';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import {constants} from '../../common/utils'
 
 const styles =  theme => ({
     card: {
@@ -90,6 +108,87 @@ class Home extends Component{
         </div>
       </div>
     );
+  }
+
+  onSearchEntered = (value) =>{
+    console.log('search value', value);
+    let filteredData = this.state.userInfo;
+    filteredData = filteredData.filter((data) =>{
+      let string = data.caption.toLowerCase();
+      let subString = value.toLowerCase();
+      return string.includes(subString);
+    })
+    this.setState({
+      userInfo: filteredData
+    })
+  }
+
+  addCommentClickHandler = (id)=>{
+    if (this.state.currentComment === "" || typeof this.state.currentComment === undefined) {
+      return;
+    }
+
+    let commentList = this.state.comments.hasOwnProperty(id)?
+      this.state.comments[id].concat(this.state.currentComment): [].concat(this.state.currentComment);
+
+    this.setState({
+      comments:{
+        ...this.state.comments,
+        [id]:commentList
+      },
+      currentComment:''
+    })
+  }
+
+
+  commentChangeHandler = (e) => {
+    this.setState({
+      currentComment:e.target.value
+    });
+  }
+
+  getBaseUserInfo = () => {
+    let that = this;
+    let url = `${constants.userInfoUrl}=${sessionStorage.getItem('access-token')}`;
+    return fetch(url,{
+      method:'GET',
+    }).then((response) =>{
+        return response.json();
+    }).then((jsonResponse) =>{
+      that.setState({
+        userInfo:jsonResponse.data
+      });
+      this.state.userInfo.map((data, index) => (
+          this.getMediaData(data.id)
+      ));
+    }).catch((error) => {
+      console.log('error user data',error);
+    });
+  }
+
+  getMediaData = (id) => {
+    let that = this;
+    let url = `${constants.userMediaUrl}/${id}?fields=id,media_type,media_url,username,timestamp&access_token=&access_token=${sessionStorage.getItem('access-token')}`;
+    return fetch(url,{
+      method:'GET',
+    }).then((response) =>{
+        return response.json();
+    }).then((jsonResponse) =>{
+      that.setState({
+        filteredData: this.state.filteredData.concat(jsonResponse)
+      })
+    }).catch((error) => {
+      console.log('error user data',error);
+    });
+  }
+
+  logout = () => {
+    sessionStorage.clear();
+    this.props.history.replace('/');
+  }
+
+  navigateToAccount = () =>{
+    this.props.history.push('/profile');
   }
 }  
 
