6a3bf7ad24212b56daad0ad32fec5fdbbdc7be36
Bhargava Raju <BhargavaR@anthology.com>
2021-06-28 00:07:33 +0530

    Adding style to home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -2,7 +2,43 @@ import React, {Component} from 'react';
 import './Home.css';
 import Header from '../../common/header/Header';
 
-
+const styles =  theme => ({
+    card: {
+      maxWidth: 1100,
+    },
+    avatar: {
+      margin: 10,
+    },
+    media: {
+      height:0,
+      paddingTop: '56.25%', // 16:9
+    },
+    formControl: {
+      display:'flex',
+      flexDirection:'row',
+      justifyContent:'space-between',
+      alignItems:'baseline',
+    },
+    comment:{
+      display:'flex',
+      alignItems:'center'
+    },
+    hr:{
+      marginTop:'10px',
+      borderTop:'2px solid #f2f2f2'
+    },
+    gridList:{
+      width: 1100,
+      height: 'auto',
+      overflowY: 'auto',
+    },
+    grid:{
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      marginTop:90
+    }
+  });
 class Home extends Component{
 
   constructor(props) {
