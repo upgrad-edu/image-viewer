74d7c9d6f4fc1fa01816e95f42ccd509e7b9a404
Bhargava Raju <BhargavaR@anthology.com>
2021-06-28 14:09:34 +0530

    Adding constructor to the Profile components

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,6 +1,31 @@
 import React, {Component} from 'react';
 
 class Profile extends Component {
+    constructor(props) {
+        super(props);
+        if (sessionStorage.getItem('access-token') == null) {
+            props.history.replace('/');
+        }
+        this.state = {
+            profile_picture: 'profile.png',
+            username: null,
+            full_name: "PGK Bhargava Raju",
+            posts: null,
+            follows: null,
+            followed_by: null,
+            editOpen: false,
+            fullNameRequired: 'dispNone',
+            newFullName: '',
+            mediaData: null,
+            imageModalOpen: false,
+            currentItem: null,
+            likeSet:new Set(),
+            comments:{},
+            filteredData:[],
+            userInfo:[],
+            likes: 3
+        }
+    }
     render(){
         return( <div>Success!</div>) 
     }
